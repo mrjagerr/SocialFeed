71191748b080da8a4d018dc92712825da0ac40bc
skyeler yearsley <s2krighty@gmail.com>
2023-10-08 14:32:54 -0500

    Creation of base form for name and post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import React, { useState } from 'react';
 
 
 function App() {
+
+  const [entries,setEntries] =useState([{name: 'Sky', post: 'Hi this is a test'}])
+
+
+
+
   return (
     <div >
-      hello world
-      
+       <form>
+        <label for='name'>Name: </label>
+        <input type='text' id='name'></input><br></br>
+        <label for= 'post'> Post: </label>
+        <input type='text' id='post'></input>
+       </form>
+     
+
+   
+    
+
     </div>
   );
 }
